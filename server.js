f7c1b44237acd5537269496d36f0dfb8c1c03f9c
ndollar <nixmail427@gmail.com>
2016-11-21 17:51:08 -0600

    Commit socket.io backend.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-
+var path = require("path");
 var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var apiRoutes = require('./app/api/routes');
 var validators = require('./app/api/validators');
+var emitter = require('./app/helpers/events').emitter;
 
-var app = express();
-
+app.use(express.static('./lib/'));
 app.use(express.static('./assets/'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,10 +17,16 @@ app.use(expressValidator({ customValidators: validators }));
 app.use('/api', apiRoutes);
 
 app.get('*', function(req, res) {
-  res.sendfile('./assets/index.html');
+  res.sendFile(path.resolve(__dirname, './assets/index.html'));
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function () {
+server.listen(port, function () {
   console.log('Listening on port', port);
+});
+
+io.on('connection', function(socket) {
+  emitter.on('io:emit', function(name, data) {
+    socket.emit(name, data);
+  });
 });
